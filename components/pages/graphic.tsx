762a1e73b343b63678fe14381d09446d4fa76377
angelaMinardi <r.minardi.angela@gmail.com>
2024-06-12 14:30:24 -0400

    Added the graphics section.

--- components/pages/graphic.tsx
+++ components/pages/graphic.tsx
@@ -1,14 +1,37 @@
-// Maybe Ari
+// Angela
 
-import React from 'react'
-import Container from '../container'
+'use client';
 
-const Graphic = () => {
-  return (
-    <section className='h-[50vh] w-screen'>
-      <Container>Maybe Ari</Container>
-    </section>
-  )
-}
+import React from 'react';
+import Image from 'next/image';
+import Container from '../container';
 
-export default Graphic
+const iconDetails = [
+  { src: '/Connect.png', alt: 'Collaboration' },
+  { src: '/Ideas.png', alt: 'Innovative Ideas' },
+  { src: '/IT.png', alt: 'Information Technology' }
+];
+
+const Graphic = () => (
+  <section className='py-8'>
+    <Container>
+      <div className='flex justify-center items-center text-white'>
+        {iconDetails.map((icon, index) => (
+          <React.Fragment key={icon.alt}>
+            <div className='text-center'>
+              <Image src={icon.src} alt={icon.alt} width={100} height={100} />
+            </div>
+            {index < iconDetails.length - 1 && (
+              <span className='text-6xl mx-4' style={{ fontFamily: 'Arial, sans-serif' }}>{'}'}</span>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+      <p className='text-center text-white mt-4 font-pressStart'>
+        {"\"Bridging the gap between students and real world experience.\""}
+      </p>
+    </Container>
+  </section>
+);
+
+export default Graphic;
